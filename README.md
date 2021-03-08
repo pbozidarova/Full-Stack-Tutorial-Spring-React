9ef1ad397e1ee391dac5b1a54e2e142c7969f48b
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-03-09 00:23:25 +0200

    Renaming

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Full-Stack-Tutorial-Spring-React
+A Simple udemy <a href="https://www.udemy.com/course/full-stack-application-with-spring-boot-and-react/">tutorial</a> that I have followed!
+
--- short-react-tutorials/react-counter/src/App.js
+++ short-react-tutorials/react-counter/src/App.js
@@ -11,6 +11,19 @@ class App extends Component {
       <div className="App">
           My Hello World!
           
+          <LearningComponents></LearningComponents>
+          
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="LearningComponents">
+          My Hello World!
+          
           <FirstComponent></FirstComponent>
           <SecondComponent></SecondComponent>
           <ThirdComponent></ThirdComponent>
@@ -19,8 +32,6 @@ class App extends Component {
     );
   }
 }
-
-
 
 
 export default App;
